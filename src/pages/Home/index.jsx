060d7a4b4af89fd08d66fd70b6a245de3de39be3
Universal0090 <idealxorezm@gmail.com>
2023-05-25 14:12:30 +0500

    new

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import "./style.scss";
+import { useState } from 'react';
 
 const index = () => {
+
+   const [count , setCount] = useState(0)
+
+   const add = () => {
+      setCount(count + 1)
+   }
+
+   const minus = () => {
+      if (count > 0 ) {
+         setCount(count - 1)
+      }
+   }
+
+
+
    return (
-      <div>
-         <h1>Home</h1>
-      </div>
+      <header> 
+         <div className="wrapper">
+            <h3>Счетчик:</h3>
+            <h1>{count}</h1>
+            <button onClick={minus} className="btn btn-danger">minus</button>
+            <button onClick={add} className="btn btn-success">add</button>
+         </div>
+      </header>
    );
 };
 
